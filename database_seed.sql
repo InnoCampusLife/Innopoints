aacf8bea902ea40afd3c8cbb70ce008c67d8b8a9
muzicius <myburningdream@gmail.com>
2016-08-25 14:32:44 +0300

    fix

--- database_seed.sql
+++ database_seed.sql
@@ -7,20 +7,20 @@ ALTER TABLE Categories AUTO_INCREMENT = 1;
 ALTER TABLE ItemCategories AUTO_INCREMENT = 1;
 ALTER TABLE Items AUTO_INCREMENT = 1;
 
-INSERT INTO Categories (id, title) VALUES (DEFAULT , 'Категория 1');
-INSERT INTO Categories (id, title) VALUES (DEFAULT , 'Категория 2');
+INSERT INTO Categories (id, title) VALUES (DEFAULT , 'Category 1');
+INSERT INTO Categories (id, title) VALUES (DEFAULT , 'Category 2');
 
-INSERT INTO Activities (id, title, comment, for_approval, type, category_id, main_option_exists, additional_exists, price) VALUES (DEFAULT , 'Активность 1', 'Комментарий 1', null, 'hourly', 1, 0, 0, 100);
-INSERT INTO Activities (id, title, comment, for_approval, type, category_id, main_option_exists, additional_exists, price) VALUES (DEFAULT , 'Активность 2', 'Комментарий 2', null, 'permanent',  2, 0,0,200);
+INSERT INTO Activities (id, title, comment, for_approval, type, category_id, main_option_exists, additional_exists, price) VALUES (DEFAULT , 'Activity 1', 'Comment 1', null, 'hourly', 1, 0, 0, 100);
+INSERT INTO Activities (id, title, comment, for_approval, type, category_id, main_option_exists, additional_exists, price) VALUES (DEFAULT , 'Activity 2', 'Comment 2', null, 'permanent',  2, 0,0,200);
 
-INSERT INTO ItemCategories (id, title) VALUES (DEFAULT , 'Одежда');
+INSERT INTO ItemCategories (id, title) VALUES (DEFAULT , 'Clothes');
 
 INSERT INTO Items (id, title, option1, value1, option2,
                    value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
-VALUES (DEFAULT, 'Рубашка поло', 'Размер', 'S', 'Цвет', 'Черный', NULL, NULL, 2, 900, 1, 0, NULL, NULL );
+VALUES (DEFAULT, 'Polo shirt', 'Size', 'S', 'Colour', 'Black', NULL, NULL, 2, 900, 1, 0, NULL, NULL );
 INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
-VALUES (DEFAULT, 'Рубашка поло', 'Размер', 'M', 'Цвет', 'Черный', NULL, NULL, 2, 900, 1, 0, NULL, 1);
+VALUES (DEFAULT, 'Polo shirt', 'Size', 'M', 'Colour', 'Black', NULL, NULL, 2, 900, 1, 0, NULL, 1);
 INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
-VALUES (DEFAULT, 'Рубашка поло', 'Размер', 'S', 'Цвет', 'Желтый', NULL, NULL, 2, 900, 1, 0, NULL, 1);
+VALUES (DEFAULT, 'Polo shirt', 'Size', 'S', 'Colour', 'Yellow', NULL, NULL, 2, 900, 1, 0, NULL, 1);
 INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
-VALUES (DEFAULT, 'Рубашка поло', 'Размер', 'M', 'Цвет', 'Желтый', NULL, NULL, 2, 900, 1, 0, NULL, 1);
+VALUES (DEFAULT, 'Polo shirt', 'Size', 'M', 'Colour', 'Yellow', NULL, NULL, 2, 900, 1, 0, NULL, 1);
