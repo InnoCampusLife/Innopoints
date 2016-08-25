1c9c7b817bd3a4c5ed1b6c816f642b49ecd34e1c
muzicius <myburningdream@gmail.com>
2016-08-25 11:02:20 +0300

    seed script has chenged

--- database_seed.sql
+++ database_seed.sql
@@ -1,4 +1,9 @@
 USE innopoints;
+TRUNCATE Categories;
+TRUNCATE Activities;
+TRUNCATE ItemCategories;
+TRUNCATE Items;
+
 
 INSERT INTO Categories (id, title) VALUES (DEFAULT , 'Категория 1');
 INSERT INTO Categories (id, title) VALUES (DEFAULT , 'Категория 2');
@@ -6,4 +11,14 @@ INSERT INTO Categories (id, title) VALUES (DEFAULT , 'Категория 2');
 INSERT INTO Activities (id, title, comment, for_approval, type, category_id, main_option_exists, additional_exists, price) VALUES (DEFAULT , 'Активность 1', 'Комментарий 1', null, 'hourly', 1, 0, 0, 100);
 INSERT INTO Activities (id, title, comment, for_approval, type, category_id, main_option_exists, additional_exists, price) VALUES (DEFAULT , 'Активность 2', 'Комментарий 2', null, 'permanent',  2, 0,0,200);
 
+INSERT INTO ItemCategories (id, title) VALUES (DEFAULT , 'Одежда');
 
+INSERT INTO Items (id, title, option1, value1, option2,
+                   value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Рубашка поло', 'Размер', 'S', 'Цвет', 'Черный', NULL, NULL, 2, 900, 1, 0, NULL, NULL );
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Рубашка поло', 'Размер', 'M', 'Цвет', 'Черный', NULL, NULL, 2, 900, 1, 0, NULL, 1);
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Рубашка поло', 'Размер', 'S', 'Цвет', 'Желтый', NULL, NULL, 2, 900, 1, 0, NULL, 1);
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Рубашка поло', 'Размер', 'M', 'Цвет', 'Желтый', NULL, NULL, 2, 900, 1, 0, NULL, 1);
