6c4e04fb675c5604e7ed67e7efea461181a7bd28
muzicius <myburningdream@gmail.com>
2017-01-31 14:04:59 +0300

    added header access control allow methods

--- routes/options_handler.rb
+++ routes/options_handler.rb
@@ -4,6 +4,7 @@ module OptionsHandler
   def self.registered(app)
     app.options '*' do
       response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+      response.headers["Access-Control-Allow-Methods"] = "HEAD, GET, PUT, POST, DELETE, OPTIONS"
       response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
       200
     end
