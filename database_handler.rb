c34576cb7b90e9e6de848161be88c0e8ba0749f3
muzicius <myburningdream@gmail.com>
2017-02-14 14:55:03 +0300

    fix thread

--- database_handler.rb
+++ database_handler.rb
@@ -1,4 +1,5 @@
 require 'mysql2'
+require 'connection_pool'
 require 'singleton'
 require_relative 'config'
 
@@ -15,7 +16,10 @@ class DatabaseHandler
   end
 
   def setup
-    @connection = Mysql2::Client.new(:host => MYSQL_DB_HOST, :port => MYSQL_DB_PORT, :username => MYSQL_DB_USER, :password => MYSQL_DB_PASSWORD, :database => MYSQL_DB_NAME, :reconnect => true)
-    @connection.query_options.merge!(:symbolize_keys => true)
+    # @connection = Mysql2::Client.new(:host => MYSQL_DB_HOST, :port => MYSQL_DB_PORT, :username => MYSQL_DB_USER, :password => MYSQL_DB_PASSWORD, :database => MYSQL_DB_NAME, :reconnect => true)
+    # @connection.query_options.merge!(:symbolize_keys => true)
+    @connection = ConnectionPool::Wrapper.new(size: 5, timeout: 5) do
+      Mysql2::Client.new(:host => MYSQL_DB_HOST, :port => MYSQL_DB_PORT, :username => MYSQL_DB_USER, :password => MYSQL_DB_PASSWORD, :database => MYSQL_DB_NAME, :reconnect => true, :symbolize_keys => true)
+    end
   end
 end
