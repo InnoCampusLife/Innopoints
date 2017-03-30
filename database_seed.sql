b09413db8264b1e4a84df33f8afd5aa15d22c868
muzicius <myburningdream@gmail.com>
2017-03-30 10:55:33 +0300

    items added

--- database_seed.sql
+++ database_seed.sql
@@ -33,15 +33,33 @@ INSERT INTO Activities (id, title, comment, for_approval, type, category_id, mai
 
 INSERT INTO ItemCategories (id, title) VALUES (DEFAULT , 'Clothes');
 INSERT INTO ItemCategories (id, title) VALUES (DEFAULT , 'Food');
+INSERT INTO ItemCategories (id, title) VALUES (DEFAULT , 'Other');
 
-INSERT INTO Items (id, title, option1, value1, option2,
-                   value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
-VALUES (DEFAULT, 'Polo shirt', 'Size', 'S', 'Colour', 'Black', NULL, NULL, 2, 900, 1, 0, NULL, NULL );
 INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
-VALUES (DEFAULT, 'Polo shirt', 'Size', 'M', 'Colour', 'Black', NULL, NULL, 2, 900, 1, 0, NULL, 1);
+VALUES (DEFAULT, 'Polo shirt', 'Size', 'S', 'Colour', 'Black', NULL, NULL, 10000, 900, 1, 0, NULL, NULL );
 INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
-VALUES (DEFAULT, 'Polo shirt', 'Size', 'S', 'Colour', 'Yellow', NULL, NULL, 2, 900, 1, 0, NULL, 1);
+VALUES (DEFAULT, 'Polo shirt', 'Size', 'M', 'Colour', 'Black', NULL, NULL, 10000, 900, 1, 0, NULL, 1);
 INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
-VALUES (DEFAULT, 'Polo shirt', 'Size', 'M', 'Colour', 'Yellow', NULL, NULL, 2, 900, 1, 0, NULL, 1);
+VALUES (DEFAULT, 'Polo shirt', 'Size', 'S', 'Colour', 'Yellow', NULL, NULL, 10000, 900, 1, 0, NULL, 1);
 INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
-VALUES (DEFAULT, 'Pizza', null, null, null, null, null, null, 2, 500, 2, 1, 2, null);
+VALUES (DEFAULT, 'Polo shirt', 'Size', 'M', 'Colour', 'Yellow', NULL, NULL, 10000, 900, 1, 0, NULL, 1);
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Pizza', null, null, null, null, null, null, 10000, 500, 2, 1, 2, null);
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Pasta', null, null, null, null, null, null, 10000, 300, 2, 1, 2, null);
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Wok', null, null, null, null, null, null, 10000, 600, 2, 1, 2, null);
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Canvas bag', 'Colour', 'Brown', NULL, NULL, NULL, NULL, 10000, 250, 3, 0, NULL, NULL );
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Canvas bag', 'Colour', 'Red', NULL, NULL, NULL, NULL, 10000, 250, 3, 0, NULL, 8 );
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Canvas bag', 'Colour', 'Brown', NULL, NULL, NULL, NULL, 10000, 250, 3, 0, NULL, 8 );
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Notebook', 'Colour', 'Brown', NULL, NULL, NULL, NULL, 10000, 250, 3, 0, NULL, NULL );
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Notebook', 'Colour', 'Grey', NULL, NULL, NULL, NULL, 10000, 250, 3, 0, NULL, 11);
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Notebook', 'Colour', 'Green', NULL, NULL, NULL, NULL, 10000, 250, 3, 0, NULL, 11);
+INSERT INTO Items (id, title, option1, value1, option2, value2, option3, value3, quantity, price, category_id, possible_joint_purchase, max_buyers, parent)
+VALUES (DEFAULT, 'Thermal mug', NULL, NULL, NULL, NULL, NULL, NULL, 10000, 450, 3, 0, NULL, NULL );
